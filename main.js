bba078da4f1f3bcdd9be93a374bb4f1183ea2c87
Fashinray <Oluwatosin5383@gmail.com>
2023-09-07 21:00:17 +0100

    Car-Rental

---
+++ main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay: 200, origin: 'top'})
+sr.reveal('.form-container form',{delay: 800, origin: 'left'})
+sr.reveal('.heading',{delay: 800, origin: 'top'})
+sr.reveal('.ride-container .box3',{delay: 600, origin: 'top'})
+sr.reveal('.services-cntainer .box3',{delay: 600, origin: 'top'})
+sr.reveal('.about-container .box3',{delay: 600, origin: 'top'})
+sr.reveal('.newsletter .box3',{delay: 400, origin: 'bottom'})
